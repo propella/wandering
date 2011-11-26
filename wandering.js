f734596607ceb4e7507c841699f5d8a1e3536949
Takashi Yamamiya <tak@metatoys.org>
2011-11-26 13:41:36 -0800

    remove right property

--- wandering.js
+++ wandering.js
@@ -1,78 +1,94 @@
-// Wandering Letters by Yoshiki Ohshima
+// Yoshiki Ohshima's Wandering Letters in Flapjax
 
-var TextFieldWidth = 300;
-// var TextFieldWidth = 150;
-var TextFieldHeight = 300;
-var SPACE_WIDTH = 0;
-var SpeedLimit = 13;
+var TextFieldWidth = 300; // Width of the text
+var TextFieldHeight = 300; // Height of initial letter positions
+var SpaceWidth = null; // Width of space initialized at loader()
+var SpeedLimit = 13; // Smallest gap which animation happens
 
-function place(pred, old) { // pred: predecessor letter
-  var newBeginning = old.beginning;
+// Return next state of the letter
+// pred : the preceding letter
+// old : old state of the letter
+function place(pred, old) {
 
+  // Test if the letter should go next line
+  var beginning = old.beginning;
   if (old.asked) {
     if(pred.isSpace) {
-      newBeginning = true;
+      beginning = true; // Beginning of the word
     } else {
-      pred.asked = true;
+      pred.asked = true; // Middle of the word TODO: unwanted side effect
     }
   }
 
-  var targetX = newBeginning ? 0 : pred.right;
-  var targetY = newBeginning ? pred.y + old.height : pred.y;
-  var curX = old.x;
-  var curY = old.y;
+  var targetX = beginning ? 0 : pred.x + pred.width;
+  var targetY = beginning ? pred.y + old.height : pred.y;
 
-  var diffX = targetX - curX;
-  var diffY = targetY - curY;
+  var diffX = targetX - old.x;
+  var diffY = targetY - old.y;
   var norm = Math.sqrt(diffX * diffX + diffY * diffY);
 
-  // The letter reached the goal
+  // Test if the letter reaches the goal
   if (norm < SpeedLimit) {
-    var newRight = targetX + old.width;
-    // The letter passed the TextFieldWidth
-    if (newRight > TextFieldWidth && pred.settled) {
-      pred.asked = true;
+    var isExceed = targetX + old.width > TextFieldWidth;
+    // The letter exceeded the TextFieldWidth
+    if (isExceed && pred.settled) {
+      pred.asked = true; // !!! TODO: unwanted side effect
     }
-    return {x: targetX, y: targetY, width: old.width, height: old.height, right: targetX + old.width, isSpace: old.isSpace, beginning: newBeginning, settled: pred.settled && !(newRight > TextFieldWidth), asked: old.asked};
+    return {x: targetX,
+            y: targetY,
+            width: old.width,
+            height: old.height,
+            isSpace: old.isSpace,
+            beginning: beginning,
+            settled: pred.settled && !isExceed,
+            asked: old.asked};
   }
+
+  // Continue wandering
   var speed = Math.min(norm, SpeedLimit);
   var speedX = (diffX/norm) * speed;
   var speedY = (diffY/norm) * speed;
-  var newX = curX + speedX;
   var jit = Math.random() * 6 - 3;
-  return {x: newX, y: curY + speedY + jit, width: old.width, height: old.height, right: newX + old.width, isSpace: old.isSpace, beginning: newBeginning, settled: false, asked: old.asked};
+
+  return {x: old.x + speedX,
+          y: old.y + speedY + jit,
+          width: old.width,
+          height: old.height,
+          isSpace: old.isSpace,
+          beginning: beginning,
+          settled: false,
+          asked: old.asked};
 }
 
 // Return a Letter structure
 function makeLetter(i) {
-
   var id = 'p' + i;
   var isSpace = $(id).firstChild.nodeValue == ' ';
-  var width = isSpace ? SPACE_WIDTH : $(id).getDimensions().width;
+  var width = isSpace ? SpaceWidth : $(id).getDimensions().width;
   var height = $(id).getDimensions().height;
 
   return {x: Math.random() * TextFieldWidth,  // X position
           y: Math.random() * TextFieldHeight, // Y position
           width: width,
           height: height,
-          right: 0, // right
           isSpace: isSpace, // True if this is a space.
           beginning: false, // True if this is the beginning of the line
-          settled: false,
-          asked: false}; // True if the word including this should go new line.
+          settled: false, // True if this animation is stopped
+          asked: false}; // True if the word including this should go next line.
 }
 
-// Return a Behavior with timer and the first anchor
+// Return a Behavior with timer and the first anchor.
+// This behavior is constant but tick at each 50 ms.
 function makeLetter0() {
   return timerB(50).liftB(
     function (time) {
+      // The anchor Letter structure
       return {x: 0,
               y: 0,
               width: 0,
               height: 0,
-              right: 0,
               isSpace: false,
-              beginning: false, // the beginning of the line
+              beginning: false,
               settled: true,
               asked: false};
     });
@@ -81,10 +97,11 @@ function makeLetter0() {
 // Return each character's behavior.
 // When it is the first character, it will be placed to 0,0.
 function makeBehavior(behaviors, i) {
-  var prevBehavior = i == 0 ? makeLetter0() : behaviors[i - 1];
-  return prevBehavior
+  var predBehavior = i == 0 ? makeLetter0() : behaviors[i - 1];
+  return predBehavior
     .changes() // Behavior a -> EventStream a
-    .collectE(makeLetter(i), function(pred, old) { return place(pred, old); })
+    .collectE(makeLetter(i),
+              function(pred, old) { return place(pred, old); })
     .startsWith({}); // (EventStream a, a) -> Behavior a
 }
 
@@ -92,7 +109,7 @@ function loader() {
 
   var ary = 'The first three years were devoted to making much smaller, simpler, and more readable versions of many of the prime parts of personal computing, including: graphics and sound, viewing/windowing, UIs, text, composition, cells, TCP/IP, etc. These have turned out well (they are chronicled in previous NSF reports and in our papers and memos).'.split('');
 
-  SPACE_WIDTH = $('space').getDimensions().width;
+  SpaceWidth = $('space').getDimensions().width;
 
   var str = '';
   for (var i = 0; i < ary.length; i++) {
